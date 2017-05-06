1ec56cbb4f2f64a40579261e15d2b04f2d5caac2
javigs82 <javigs82@gmail.com>
2017-05-06 16:29:56 +0200

    Hello World Restful service

---
+++ src/main/java/es/javier/services/SpringApplicationBuilder.java
@@ -0,0 +1,12 @@
+package es.javier.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationBuilder {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationBuilder.class, args);
+	}
+}
